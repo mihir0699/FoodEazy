24af4414e957a46c05a8059a3cb4c71cfce9d913
Mihir <mihir0699@gmail.com>
2020-11-17 14:01:57 +0530

    Deploying'

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/api/get_orders", require("./routes/orders"));
 app.use(express.static(path.join(__dirname, '/build/')))
 
 app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
 })
 
 const port = process.env.PORT || 5000;
